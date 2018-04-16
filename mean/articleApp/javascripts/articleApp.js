dc032fa56836a97b503b71a1cfc0b5c1658bd8e9
EdilsonDiasAlves <edilson@jaya-apps.com>
2018-04-16 19:28:35 -0300

    Primeiro commit da aplicacao mean - Angular e node

---
+++ mean/articleApp/javascripts/articleApp.js
@@ -0,0 +1,12 @@
+app = angular.module('articleApp', []);
+
+app.controller('mainController', function($scope){
+    $scope.articles = [];
+    $scope.newArticle = {username: '', title: '', text: '', timestamp: ''};
+
+    $scope.post = function(){
+        $scope.newArticle.timestamp = Date.now();
+        $scope.articles.push($scope.newArticle);
+        $scope.newArticle = {username: '', title: '', text: '', timestamp: ''};
+    };
+});
